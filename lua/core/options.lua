ced045b4f28a07426ee5f4c2906f8dc420805c96
Paulo Patto <paulopatto@paulopatto.com>
2023-07-22 15:34:18 -0300

    wip(neovim): Writting configs
    - https://github.com/fabioaacarneiro/neovim-lua-fc-setup
    - https://youtu.be/opxRKZn54kk

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,22 +3,38 @@
 -- vim.g.mapleader      = ' ' 
 -- vim.g.maplocalleader = ' '
 
+
+-- [ GENERAL ]
 vim.opt.backspace  = '2'
 vim.opt.showcmd    = true
 vim.opt.laststatus = 2
 vim.opt.autowrite  = true
-vim.opt.cursorline = true
 vim.opt.autoread   = true
+vim.opt.mouse      = 'a'           -- enable mouse for all
+--vim.cmd [[ set noswapfile ]]
+vim.opt.swapfile   = false         -- Disable swap file
+vim.opt.history    = 100
+vim.opt.wildignore:append("*/node_modules/*,*/vendor/*,*/venv/*,*/.venv/*,*/target/*")
 
---Line numbers
--- equivalente ao `set number`
-vim.wo.number = true
 
+-- [ THEME ]
+vim.opt.termguicolors = true
+vim.cmd [[ colorscheme onedark ]] 
+
+
+-- [ EDITOR ]
 -- use spaces for tabs and whatnot
-vim.opt.tabstop    = 2
-vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab  = true
-
-vim.cmd [[ set noswapfile ]]
-
+vim.opt.tabstop    = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.wo.number = true -- vim.opt.number = true
+vim.opt.signcolumn = 'yes' -- Extra left colum
+vim.opt.showmatch = true -- Show match parentesis
+vim.opt.foldmethod = 'marker'
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.colorcolumn = '80'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
