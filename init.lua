65a258e52197dae031662ff7de5ee6eb1fb5298e
Paulo Patto <paulopatto@paulopatto.com>
2024-04-01 23:23:16 -0300

    feat: Split plugins configs

--- init.lua
+++ init.lua
@@ -17,18 +17,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6', 
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-}
-local opts    = {}
-
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 -- Telescope configs
 local builtin = require("telescope.builtin")
@@ -50,6 +39,10 @@ treesitter_configs.setup({
     "typescript", 
   },
 })
+
+
+-- NeoTree
+vim.keymap.set('n', '<leader>p', ':Neotree filesystem reveal left<CR>', {})
 
 -- Colorscheme
 require("catppuccin").setup()
