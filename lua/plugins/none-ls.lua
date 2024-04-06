f631be4cfe616f5d5b07725e724c59fc73f58902
Paulo Patto <paulopatto@paulopatto.com>
2024-04-06 20:30:09 -0300

    feat: Config LSP linters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,7 +4,26 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+				-- Lua language
 				null_ls.builtins.formatting.stylua,
+
+				-- Ruby Stack
+				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.diagnostics.rubocop,
+
+        -- JavaScript / Typescript Stack
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.eslint_d.with ({
+          method = null_ls.methods.DIAGNOSTICS_ON_SAVE
+        }),
+
+
+        -- Python
+        -- link: https://www.reddit.com/r/neovim/comments/1069wto/what_python_lsp_and_linter/
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.isort.with({ --/flake8
+          method = null_ls.methods.DIAGNOSTICS_ON_SAVE
+        })
 			},
 		})
 
