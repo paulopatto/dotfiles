af58ef865bed7ccb7aaa1ef446e7ad7d68ce6aa4
Paulo Patto <paulopatto@paulopatto.com>
2024-04-09 16:33:59 -0300

    feat(neovim): Change file explorer
    - changes git modifications icons
    - ignore some files

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,38 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
+    require('neo-tree').setup({
+      close_if_last_window = true, -- - Close Neo-tree if it is the last window left in the tab
+      filesystem = {
+        filtered_items = {
+          never_show = {
+            ".DS_Store",
+            ".git",
+            "thumbs.db",
+          },
+          hide_hidden = true, -- only works on Windows for hidden files/directories
+          hide_gitignored = true,
+          hide_by_name = {
+            "node_modules"
+          },
+        },
+      },
+      git_status = {
+        symbols = {
+          -- Change type
+          added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+          modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+          deleted   = "✖",-- this can only be used in the git_status source
+          renamed   = "󰁕",-- this can only be used in the git_status source
+          -- Status type
+          untracked = "",
+          ignored   = "",
+          unstaged  = "󰄱",
+          staged    = "",
+          conflict  = "",
+        }
+      },
+    })
     -- NeoTree
     vim.keymap.set('n', '<leader>p', ':Neotree filesystem reveal left<CR>', {})
   end
