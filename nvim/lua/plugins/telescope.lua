a5cd300cde01a768080f063206990e8cfa2de9ed
Paulo Patto <paulopatto@paulopatto.com>
2024-04-09 16:32:43 -0300

    feat(neovim): Config telescope to ignore some files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       -- Telescope configs
-      local builtin = require("telescope.builtin").setup({
+      require("telescope").setup({
         file_ignore_patterns = {
           "node_modules/.*",
           ".git/.*",
@@ -15,6 +15,8 @@ return {
           "yarn.lock"
         },
       })
+
+      local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
