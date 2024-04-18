ad52cc68cda7d0cc1417c1fc142213bdffe67efe
Paulo Patto <paulopatto@paulopatto.com>
2024-04-18 17:27:45 -0300

    feat(neovim): Add some LSP commands
    Add keymap to go references (`[NORMAL MODE] gr`)

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -47,7 +47,9 @@ return {
 			-- Read more: https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) -- Will open helper pop-up
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
