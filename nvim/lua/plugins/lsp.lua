dd49759cf1e3184201ae4e76fbfca165005f5d75
Paulo Patto <paulopatto@paulopatto.com>
2024-04-19 01:45:41 -0300

    feat(neovim): Add LSP reame command
    from video: [3 levels of vim refactoring](https://youtu.be/oQB8lYUZtrY?si=2niuH8nzBfq_FiQv)

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -49,7 +49,7 @@ return {
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
