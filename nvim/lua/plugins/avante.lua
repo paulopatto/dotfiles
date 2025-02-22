a9a37c348f36f896d1d0ffe710648d1bb9c6bfec
Paulo Patto <paulopatto@paulopatto.com>
2025-02-22 13:39:21 -0300

    feat(neovim): Config Avante.nvim to user OpenAI as default provider

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -7,7 +7,6 @@ return {
     ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
     -- add any opts here
     -- for example
-    provider = "openai",
     openai = {
       endpoint = "https://api.openai.com/v1",
       model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
@@ -15,6 +14,80 @@ return {
       temperature = 0, -- adjust if needed
       max_tokens = 4096,
     },
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-3-5-sonnet-20241022",
+      temperature = 0,
+      max_tokens = 4096,
+    },
+    provider = "openai", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+    -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+    -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+    -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
+    auto_suggestions_provider = "openai",
+
+    ---Specify the special dual_boost mode
+    ---1. enabled: Whether to enable dual_boost mode. Default to false.
+    ---2. first_provider: The first provider to generate response. Default to "openai".
+    ---3. second_provider: The second provider to generate response. Default to "claude".
+    ---4. prompt: The prompt to generate response based on the two reference outputs.
+    ---5. timeout: Timeout in milliseconds. Default to 60000.
+    ---How it works:
+    --- When dual_boost is enabled, avante will generate two responses from the first_provider and second_provider respectively. Then use the response from the first_provider as provider1_output and the response from the second_provider as provider2_output. Finally, avante will generate a response based on the prompt and the two reference outputs, with the default Provider as normal.
+    ---Note: This is an experimental feature and may not work as expected.
+    dual_boost = {
+      enabled = false,
+      first_provider = "openai",
+      second_provider = "claude",
+      prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+      timeout = 60000, -- Timeout in milliseconds
+    },
+
+    behaviour = {
+      auto_suggestions = false, -- Experimental stage
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = false,
+      minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+      enable_token_counting = true, -- Whether to enable token counting. Default to true.
+      enable_cursor_planning_mode = false, -- Whether to enable Cursor Planning Mode. Default to false.
+    },
+    
+    mappings = {
+      --- @class AvanteConflictMappings
+      diff = {
+        ours = "co",
+        theirs = "ct",
+        all_theirs = "ca",
+        both = "cb",
+        cursor = "cc",
+        next = "]x",
+        prev = "[x",
+      },
+      suggestion = {
+        accept = "<M-l>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+      jump = {
+        next = "]]",
+        prev = "[[",
+      },
+      submit = {
+        normal = "<CR>",
+        insert = "<C-s>",
+      },
+      sidebar = {
+        apply_all = "A",
+        apply_cursor = "a",
+        switch_windows = "<Tab>",
+        reverse_switch_windows = "<S-Tab>",
+      },
+  },
+
+
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
