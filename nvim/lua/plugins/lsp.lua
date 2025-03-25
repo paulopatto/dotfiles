395ad8e5c0333ca56e285f2e5497803c4de6b9a3
Paulo Patto <paulopatto@paulopatto.com>
2025-03-25 20:05:49 -0300

    feat(neovim): Python DAP
    Add support to python dap (via nvim-dap-python and debugpy).

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,142 +1,146 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        -- Availables LSP Servers
-        -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-        ensure_installed = {
-          "jdtls",
-          "kotlin_language_server",
-          "lua_ls",
-          "pyright",
-          "ruff",
-          "solargraph",
-          "tailwindcss",
-          "terraformls",
-          "ts_ls",
-        },
-      })
-    end,
-  },
-  {
-    "mfussenegger/nvim-jdtls",
-    ft = { "java" }, -- Carrega apenas para arquivos Java
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "mfussenegger/nvim-jdtls",
-    },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      local java_path = vim.fn.expand("~/.asdf/shims/java")
-      local jdtls = require("jdtls")
-      local on_attach = require("cmp_nvim_lsp").on_attach
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ensure_installed = {
+					"debugpy",
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				-- Availables LSP Servers
+				-- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
+				ensure_installed = {
+					"jdtls",
+					"kotlin_language_server",
+					"lua_ls",
+					"pyright",
+					"ruff",
+					"solargraph",
+					"tailwindcss",
+					"terraformls",
+					"ts_ls",
+				},
+			})
+		end,
+	},
+	{
+		"mfussenegger/nvim-jdtls",
+		ft = { "java" }, -- Carrega apenas para arquivos Java
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mfussenegger/nvim-jdtls",
+		},
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			local java_path = vim.fn.expand("~/.asdf/shims/java")
+			local jdtls = require("jdtls")
+			local on_attach = require("cmp_nvim_lsp").on_attach
 
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        filetypes = { "python" },
-      })
-      lspconfig.ruff.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        filetypes = { "python" },
-      })
-      lspconfig.kotlin_language_server.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.jdtls.setup({
-        cmd = { "jdtls" },
-        root_dir = lspconfig.util.root_pattern("pom.xml", "build.gradle", ".git"), -- Define o diretório raiz do projector
-        settings = {
-          java = {
-            configuration = {
-              runtimes = {
-                {
-                  name = "JavaSE-17",
-                  path = java_path, -- Usa o caminho do JDK gerenciado pelo asdf
-                },
-              },
-            },
-          },
-        },
-      })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = { "python" },
+			})
+			lspconfig.ruff.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = { "python" },
+			})
+			lspconfig.kotlin_language_server.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jdtls.setup({
+				cmd = { "jdtls" },
+				root_dir = lspconfig.util.root_pattern("pom.xml", "build.gradle", ".git"), -- Define o diretório raiz do projector
+				settings = {
+					java = {
+						configuration = {
+							runtimes = {
+								{
+									name = "JavaSE-17",
+									path = java_path, -- Usa o caminho do JDK gerenciado pelo asdf
+								},
+							},
+						},
+					},
+				},
+			})
 
-      jdtls.start_or_attach({
-        cmd = { vim.fn.expand("$HOME/.local/share/nvim/mason/bin/jdtls") },                              -- Caminho do jdtls
-        root_dir = vim.fs.dirname(vim.fs.find({ "pom.xml", "build.gradle", ".git" }, { upward = true })[1]), -- Define o diretório raiz do projeto
-        settings = {
-          java = {
-            configuration = {
-              runtimes = {
-                {
-                  name = "JavaSE-17",
-                  path = java_path, -- Usa o caminho do JDK gerenciado pelo asdf
-                },
-              },
-            },
-          },
-        },
-      })
+			jdtls.start_or_attach({
+				cmd = { vim.fn.expand("$HOME/.local/share/nvim/mason/bin/jdtls") }, -- Caminho do jdtls
+				root_dir = vim.fs.dirname(vim.fs.find({ "pom.xml", "build.gradle", ".git" }, { upward = true })[1]), -- Define o diretório raiz do projeto
+				settings = {
+					java = {
+						configuration = {
+							runtimes = {
+								{
+									name = "JavaSE-17",
+									path = java_path, -- Usa o caminho do JDK gerenciado pelo asdf
+								},
+							},
+						},
+					},
+				},
+			})
 
-      lspconfig.terraformls.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.terraformls.setup({
+				capabilities = capabilities,
+			})
 
-      -- Buffer local mappings.
-      -- See `:help vim.lsp.*` for documentation on any of the below functions
-      -- Read more: https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) -- Will open helper pop-up
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			-- Buffer local mappings.
+			-- See `:help vim.lsp.*` for documentation on any of the below functions
+			-- Read more: https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {}) -- Will open helper pop-up
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-      -- Format on save
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("Format", { clear = true }),
-        callback = function()
-          vim.lsp.buf.format()
-        end,
-      })
+			-- Format on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("Format", { clear = true }),
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
 
-      -- Add blankline at EOF on save
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("AddBlankLine", { clear = true }),
-        callback = function()
-          local last_line = vim.api.nvim_buf_line_count(0)
-          local last_line_content = vim.api.nvim_buf_get_lines(0, last_line - 1, last_line, false)[1]
-          if last_line_content ~= "" then
-            vim.api.nvim_buf_set_lines(0, last_line, last_line, false, { "" })
-          end
-        end,
-      })
+			-- Add blankline at EOF on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("AddBlankLine", { clear = true }),
+				callback = function()
+					local last_line = vim.api.nvim_buf_line_count(0)
+					local last_line_content = vim.api.nvim_buf_get_lines(0, last_line - 1, last_line, false)[1]
+					if last_line_content ~= "" then
+						vim.api.nvim_buf_set_lines(0, last_line, last_line, false, { "" })
+					end
+				end,
+			})
 
-      -- Organize import
-      vim.keymap.set({ "n", "v" }, "<leader>oi", function()
-        vim.lsp.buf.code_action({
-          context = {
-            only = { "source.organizeImports" },
-          },
-        })
-      end)
-    end,
-  },
+			-- Organize import
+			vim.keymap.set({ "n", "v" }, "<leader>oi", function()
+				vim.lsp.buf.code_action({
+					context = {
+						only = { "source.organizeImports" },
+					},
+				})
+			end)
+		end,
+	},
 }
 
