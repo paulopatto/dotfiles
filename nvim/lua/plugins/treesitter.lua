041e9284e8f4fe287e81b2a75a458b61d94f6a39
Paulo Patto <paulopatto@paulopatto.com>
2025-03-25 21:19:25 -0300

    fix(neovim): Extra highlight for ruby in treesitter
    Som languages depend on vim's regex highlightin system (such ruby) for
    ident rules.
    
    FROM: https://youtu.be/IobijoroGE0?si=nvq16kpJG0K0x6wa

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,28 @@
 -- Treesitter configs
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local treesitter_configs = require("nvim-treesitter.configs")
-    treesitter_configs.setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      ensure_installed = {
-        "c",
-        "html",
-        "java",
-        "javascript",
-        "kotlin",
-        "lua",
-        "python",
-        "ruby",
-        "typescript",
-      },
-      auto_install = true,
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local treesitter_configs = require("nvim-treesitter.configs")
+		treesitter_configs.setup({
+			highlight = {
+				enable = true,
+				additiona_vim_regex_highlighting = { "ruby" },
+			},
+			indent = { enable = true },
+			ensure_installed = {
+				"c",
+				"html",
+				"java",
+				"javascript",
+				"kotlin",
+				"lua",
+				"python",
+				"ruby",
+				"typescript",
+			},
+			auto_install = true,
+		})
+	end,
 }
 
