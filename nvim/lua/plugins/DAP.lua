8cff62efe2e9872fafda1d9a71fa99f83b506ac2
Paulo Patto <paulopatto@paulopatto.com>
2025-05-02 16:54:25 -0300

    wip: Try enable run file via DAP

--- nvim/lua/plugins/DAP.lua
+++ nvim/lua/plugins/DAP.lua
@@ -28,6 +28,10 @@ return {
     config = function(_, opts)
       --[[ local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
       require("dap-python").setup(path) ]]
+      local dap = require("dap-python")
+
+      -- calls dap python run file
+      vim.keymap.set("n", "<leader>dpr", dap.test_method, {})
     end,
   },
   {
