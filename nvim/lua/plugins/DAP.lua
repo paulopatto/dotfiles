2519f6e95a09baa425fe46b08ace9720093a7ce4
Paulo Patto <paulopatto@paulopatto.com>
2025-05-03 19:09:50 -0300

    fix typos

--- nvim/lua/plugins/DAP.lua
+++ nvim/lua/plugins/DAP.lua
@@ -4,31 +4,6 @@ return {
     config = function()
       local dap = require("dap")
 
-      dap.adapters.java = {
-        type = 'executable',
-        command = "$HOME/.local/share/nvim/mason/bin/vscode-java-debug",
-        options = {
-          cwd = vim.fn.getcwd()
-        }
-      }
-
-      dap.configurations.java = {
-        {
-          name = "Launch Current File",
-          type = "java",
-          request = "launch",
-          mainClass = "${file}",
-        },
-        {
-          name = 'Attach to Remote JVM',
-          type = 'java',
-          request = 'attach',
-          hostName = 'localhost',
-          port = 5005,
-        },
-      }
-    }
-
       -- Key maps
       -- See :help dap.txt, :help dap-mapping and :help dap-api.
       -- debug toogle (breakpoint)
@@ -41,6 +16,30 @@ return {
       vim.keymap.set("n", "<Leader>di", dap.step_into, {})
       -- debug repl: Inspecting the state via the built-in REPL
       vim.keymap.set("n", "<Leader>dr", dap.repl.open, {})
+
+      dap.adapters.java = {
+        type = "executable",
+        command = "$HOME/.local/share/nvim/mason/bin/vscode-java-debug",
+        options = {
+          cwd = vim.fn.getcwd(),
+        },
+      }
+
+      dap.configurations.java = {
+        {
+          name = "Launch Current File",
+          type = "java",
+          request = "launch",
+          mainClass = "${file}",
+        },
+        {
+          name = "Attach to Remote JVM",
+          type = "java",
+          request = "attach",
+          hostName = "localhost",
+          port = 5005,
+        },
+      }
     end,
   },
   {
@@ -89,18 +88,18 @@ return {
       local dap_virtual_text = require("nvim-dap-virtual-text")
 
       dap_virtual_text.setup({
-        enabled = true,      -- enable this plugin (the default)
+        enabled = true, -- enable this plugin (the default)
         enabled_commands = true, -- create commands
         -- * DapVirtualTextEnable
         -- * DapVirtualTextDisable
         -- * DapVirtualTextToggle
-        highlight_changed_variables = true,                             -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-        highlight_new_as_changed = false,                               -- highlight new variables in the same way as changed variables
-        show_stop_reason = true,                                        -- show stop reason when stopped for exceptions
-        commented = false,                                              -- prefix virtual text with comment string
-        only_first_definition = true,                                   -- only show virtual text at first definition (if there are multiple)
-        all_references = false,                                         -- show virtual text on all all references of the variable (not only definitions)
-        clear_on_continue = false,                                      -- clear virtual text on "continue" (might cause flickering when stepping)
+        highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+        highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables
+        show_stop_reason = true, -- show stop reason when stopped for exceptions
+        commented = false, -- prefix virtual text with comment string
+        only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+        all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+        clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
         virt_text_pos = vim.fn.has("nvim-0.10") == 1 and "inline" or "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
       })
     end,
