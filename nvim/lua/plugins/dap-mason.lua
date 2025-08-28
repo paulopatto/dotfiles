5379b310a009f34c032f654cdb17e79616170584
Paulo Patto <paulopatto@paulopatto.com>
2025-08-28 14:36:36 -0300

    feat(nvim): add DAP support and related plugins
    This commit introduces support for the Debug Adapter Protocol (DAP) in Neovim, enabling debugging capabilities.
    
    The following plugins were added and configured:
    - dap-mason: Manages DAP installations using Mason.
    - dap-python: Provides Python-specific DAP support.
    - dap-ui: Offers a user interface for DAP.
    - dap-virtual-text: Displays debug information inline using virtual text.
    
    The DAP.lua plugin was modified to utilize mason-nvim-dap for automatic installation of debug adapters. Additionally, gitsigns.lua was updated, and plenary.lua was added as a dependency for other plugins.

---
+++ nvim/lua/plugins/dap-mason.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "williamboman/mason.nvim",
+    },
+    opts = {
+      -- This line is essential to making automatic installation work
+      -- :exploding-brain
+      -- https://www.johntobin.ie/blog/debugging_in_neovim_with_nvim-dap/
+      handlers = {},
+      automatic_installation = {
+        exclude = {
+          "python",
+        },
+        ensure_installed = {
+          "python",
+          "debugpy",
+        },
+      },
+    },
+    config = function()
+      require("mason-nvim-dap").setup({
+        automatic_installation = true,
+        ensure_installed = {
+          "python",
+          "debugpy",
+        },
+      })
+    end,
+  },
+}
+
