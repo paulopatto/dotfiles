34cbc227b148dcb7e914a49df20a3be42aec9d77
Paulo Patto <paulopatto@paulopatto.com>
2025-08-28 15:17:13 -0300

    Add command to open lazygit and lazydocker

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,35 @@
 -- Treesitter configs
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local treesitter_configs = require("nvim-treesitter.configs")
-    treesitter_configs.setup({
-      highlight = {
-        enable = true,
-        additiona_vim_regex_highlighting = { "ruby" },
-      },
-      indent = { enable = true },
-      ensure_installed = {
-        "c",
-        "html",
-        "java",
-        "javascript",
-        "kotlin",
-        "lua",
-        "python",
-        "ruby",
-        "typescript",
-        "bash",
-        "diff",
-        "luadoc",
-        "markdown",
-      },
-      auto_install = true,
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local treesitter_configs = require("nvim-treesitter.configs")
+		treesitter_configs.setup({
+			highlight = {
+				enable = true,
+				additiona_vim_regex_highlighting = { "ruby" },
+			},
+			indent = { enable = true },
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"java",
+				"javascript",
+				"kotlin",
+				"lua",
+				"luadoc",
+				"markdown",
+				"python",
+				"query",
+				"ruby",
+				"tsx",
+				"typescript",
+				"yaml",
+			},
+			auto_install = true,
+		})
+	end,
 }
 
