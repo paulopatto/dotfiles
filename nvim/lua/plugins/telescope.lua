8a2117958a660c5c45b7882d4a293804346dec04
Paulo Patto <paulopatto@paulopatto.com>
2025-08-28 18:26:27 -0300

    ignoring compiled files at Telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       -- Telescope configs
       require("telescope").setup({
@@ -12,31 +13,34 @@ return {
           "pnpm-lock.yaml",
           "dump/.*",
           "lazy-lock.json",
-          "yarn.lock"
+          "yarn.lock",
+          "**/*.class",
+          "**/.pyc",
         },
       })
 
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    end,
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       -- This is your opts table
       require("telescope").setup({
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
-          }
-        }
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
       })
       -- To get ui-select loaded and working with telescope, you need to call
       -- load_extension, somewhere after setup function:
       require("telescope").load_extension("ui-select")
-    end
-  }
+    end,
+  },
 }
+
