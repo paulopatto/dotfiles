e37876836b3bb1a0517ce84edd2e8a908ec3098d
Paulo Patto <paulopatto@paulopatto.com>
2025-09-22 16:01:48 -0300

    chore(nvim): Changes ondark to not default theme (#84)

--- nvim/lua/plugins/onedark-theme.lua
+++ nvim/lua/plugins/onedark-theme.lua
@@ -1,11 +1,10 @@
 return {
   "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require("onedark").setup({
+    --[[ require("onedark").setup({
       style = "darker",
     })
     -- Enable theme
-    require("onedark").load()
+    require("onedark").load() ]]
   end,
 }
