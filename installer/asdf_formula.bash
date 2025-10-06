3be75f45ef140bf2fa554e35050ef0606cbab588
Paulo Patto <paulopatto@paulopatto.com>
2025-10-06 19:31:44 -0300

    fix: asdf version

--- installer/asdf_formula.bash
+++ installer/asdf_formula.bash
@@ -31,7 +31,7 @@ function install_asdf() {
 
   # Opcional: mostra conteúdo extraído
   echo "Conteúdo extraído:"
-  ls "$INSTALL_DIR/" | grep "asdf"
+  ls "$INSTALL_DIR/asdf-$VERSION"
   ln -sf "$INSTALL_DIR/asdf-$LATEST_TAG" "$INSTALL_DIR/asdf"
 
   # Limpeza
