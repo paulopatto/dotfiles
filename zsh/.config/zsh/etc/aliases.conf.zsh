f8b9d5e3448d013a6fe8e6e9b59a2418f2e19334
Paulo Patto <paulopatto@paulopatto.com>
2026-07-15 22:05:13 -0300

    feat(nvim): Add bash alias to zen mode of Avante.nvim

--- zsh/.config/zsh/etc/aliases.conf.zsh
+++ zsh/.config/zsh/etc/aliases.conf.zsh
@@ -14,3 +14,7 @@ alias ipy="python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance
 if which tree >/dev/null 2>&1; then
     alias lt='tree -I .gitignore'
 fi
+
+# Avante nvim zen mode
+# FROM: https://www.reddit.com/r/neovim/comments/1n85xuz/avantenvim_acp_avante_zen_mode/
+alias avante='nvim -c "lua vim.defer_fn(function()require(\"avante.api\").zen_mode()end, 100)"'
